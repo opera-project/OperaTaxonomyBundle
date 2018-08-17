26608fc46f0cf0e58fe0178beeb62d7d906c8dce
Nsy <noelie@donkeycode.com>
2018-08-17 12:08:04 +0200

    first commit init

---
+++ OperaTaxonomyBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Opera\TaxonomyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class OperaTaxonomyBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+TaxonomyBundle
+=============
+
+The `OperaTaxonomyBundle` provide a way to tag entities for opera-project cms
+
+License
+=======
+
+This bundle is released under the [MIT license](LICENSE)
